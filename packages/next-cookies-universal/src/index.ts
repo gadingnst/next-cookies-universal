11099cb6031e016b750b475fc1a8050f05bb9c15
Gading Nasution <account@gading.dev>
2023-06-30 21:20:52 +0700

    fix: 0.1.0

--- packages/next-cookies-universal/src/index.ts
+++ packages/next-cookies-universal/src/index.ts
@@ -1,7 +1,6 @@
 /* eslint-disable @typescript-eslint/no-var-requires */
 /** Cookie.ts */
-// import ICookieServer from './Cookies.server';
-// import ICookieClient from './Cookies.client';
+
 import type { BaseCookies, CookiesContext } from './Cookies.interface';
 
 function Cookies(ctx: CookiesContext = 'server'): BaseCookies {
